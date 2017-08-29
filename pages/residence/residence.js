f4108cb3118052c19a008aa225884a38900a5770
佘国榛 <15201752137@163.com>
2017-08-30 04:45:33 +0800

    客户端基本完成，有空重构一下？ 也许吧

--- pages/residence/residence.js
+++ pages/residence/residence.js
@@ -1,70 +1,208 @@
-// residence.js
-Page({
+// test.js
+var util = require('../../utils/util.js');
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
-    pics:[]
+    files: [],
+    FilePaths: [],
+    date: "",
+    time: "12:01",
+    location: "",
+    organization: "",
+    joinNumber: "", //参加人数
+    name: "",     //发声者名称
+    feeling: "",  //感受 
+    theme: "",
+    content: "",
+    other: "",
+
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //上传活动时候的照片
+  chooseActivityImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+        var tempFilePathDic = {};
+        tempFilePathDic['path'] = res.tempFilePaths[0];
+        tempFilePathDic['type'] = 'activity',
+
+          console.log(typeof tempFilePathDic);
+        that.setData({
+          FilePaths: that.data.FilePaths.concat(tempFilePathDic)
+        });
+
+      }
+    })
+  },
+
+  //上传微创投表图片
+  chooseSheetImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+        var tempFilePathDic = {};
+        tempFilePathDic['path'] = res.tempFilePaths[0];
+        tempFilePathDic['type'] = 'sheet',
+
+          console.log(typeof tempFilePathDic);
+        that.setData({
+          FilePaths: that.data.FilePaths.concat(tempFilePathDic)
+        });
+      }
+    })
+  },
+
   onLoad: function (options) {
-  
+
+    console.log(util.getNowFormatDate());
+    this.setData({
+      date: util.getNowFormatDate()
+    });
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
+
+  bindOrganizationChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      organization: e.detail.value
+    });
+  },
+
+  bindLocationChange: function (e) {
+    this.setData({
+      location: e.detail.value
+    });
+  },
+
+  bindJoinNumberChange: function (e) {
+    this.setData({
+      joinNumber: e.detail.value
+    });
+  },
+
+  bindThemeChange: function (e) {
+    this.setData({
+      theme: e.detail.value
+    });
+  },
+
+  bindNameChange: function (e) {
+    this.setData({
+      name: e.detail.value
+    });
+  },
+
+  bindFeelingChange: function (e) {
+    console.log("feeling");
+    this.setData({
+      feeling: e.detail.value
+    })
+  },
+
+
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+
+  bindContentChange: function (e) {
+    console.log("content");
+    this.setData({
+      content: e.detail.value
+    });
+  },
+
+  bindOtherChange: function (e) {
+    this.setData({
+      other: e.detail.value
+    })
+  },
+
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+
+
   uploadDIY(filePaths, successUp, failUp, i, length) {
+    var that = this;
+    console.log("uploadDIY");
     wx.uploadFile({
-      url: 'https://45053688.hazelnutsgz.com:444/api/upload',
-      filePath: filePaths[i],
+      url: 'https://45053688.hazelnutsgz.com:444/residence/pic',
+      filePath: filePaths[i].path,
       name: 'file',
       formData: {
-        'pictureUid': "HHH",
-        'pictureAid': "LLL"
+        'date': that.data.date,
+        'time': that.data.time,
+        'organization': that.data.organization,
+        "type": filePaths[i]['type']
       },
       success: (resp) => {
         successUp++;
@@ -76,7 +214,7 @@ Page({
         i++;
         if (i == length) {
           wx.showToast({
-            title: '总共'+successUp+'张上传成功, '+failUp+'张上传失败！',
+            title: '总共' + successUp + '张上传成功, ' + failUp + '张上传失败！',
           })
         }
         else {  //递归调用uploadDIY函数
@@ -87,28 +225,92 @@ Page({
     });
   },
 
-  submitAll: function(e){
-    console.log(e.detail.value);
-    console.log("GGG");
-    wx.chooseImage({
-      count: 9,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: (res) => {
 
+  submitAll: function () {
+    console.log(this.data);
+    var that = this;
+    if (!this.validate()) {
+      return;
+    }
+    console.log("准备开始request words");
+    wx.request({
+      url: 'https://45053688.hazelnutsgz.com:444/residence/words',
+      method: "POST",
+      data: {
+        date: that.data.date,      //日期
+        time: that.data.time,       //时间
+        location: that.data.location,      //位置
+        organization: that.data.organization, //组织名称
+        joinNumber: that.data.joinNumber, //参加人数
+        name: that.data.name,     //发声者名称
+        feeling: that.data.feeling,  //感受 
+        theme: that.data.theme,       //主题
+        content: that.data.content,   //内容
+        other: that.data.other        //其他背景
+      },
+      success: function (res) {
+        console.log(res);
+        wx.showToast({
+          title: '正在上传图片，请先不要关闭',
+        })
         var successUp = 0; //成功个数
         var failUp = 0; //失败个数
-        var length = res.tempFilePaths.length; //总共个数
+        var length = that.data.FilePaths.length; //总共个数
         var i = 0; //第几个
-        this.uploadDIY(res.tempFilePaths, successUp, failUp, i, length);
+        that.uploadDIY(that.data.FilePaths, successUp, failUp, i, length);
       },
-    });
+      complete: function (res) {
+        console.log("complete");
+      },
+    }),
+      console.log("KKK");
+
+
+
+
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  validate: function () {
+
+    //验证文字
+    for (var key in this.data) {
+      if (key == "__webviewId__") {
+        continue;
+      }
+      if (this.data[key] == [] || this.data[key] == "") {
+        wx.showToast({
+          title: key + " 还没有填写，请补充完整后再提交"
+        })
+        return false;
+      }
+    }
+
+    //验证图片是否上传
+    var hasActivity = false;
+    var hasSheet = false;
+    for (var pic in this.data.FilePaths) {
+      console.log(pic);
+      if (this.data.FilePaths[pic]['type'] == 'activity') {
+        hasActivity = true;
+      } if (this.data.FilePaths[pic]['type'] == 'sheet') {
+        hasSheet = true;
+      } if (hasSheet && hasActivity) {
+        return true;
+      }
+    }
+    wx.showToast({
+      title: '图片没有上传，请选择图片之后再上传',
+    })
+    return false;
+
+  },
+
+
+
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
   }
-})
+});
--- pages/worker/worker.js
+++ pages/worker/worker.js
@@ -1,66 +1,357 @@
-// worker.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    radioxmsjhItems: [
+      { name: '在项目书计划中', value: '在项目书计划中' },
+      { name: '未在项目书计划中', value: '未在项目书计划中', checked: true }
+    ],
+    radiofwdxItems: [
+      { name: '是服务对象', value: '是服务对象' },
+      { name: '不是服务对象', value: '不是服务对象', checked: true }
+    ],
+
+
+    zzmc: "", //组织名称
+    sssq: "", //所属社区
+    gfdd: "", //跟访地点
+    gfsj: "", //跟访时间
+    gfrxm: "", //跟访人姓名
+    gfsc: "", //跟访时长
+    gfxs: "", //跟访形式
+    hdmc: "",  //活动名称
+    xmsjh: "未在项目书计划中", //（不是）项目书计划
+    fwdx: "不是服务对象", //（不是）服务对象
+    hdmb: "", //活动目标
+    hdlc: "", //活动流程
+    yd: "",   //优点
+    qd: "", //缺点
+    yhjy: "", //优化建议
+    grgs: "", //个人感受
+    zzzxq: "", //自组织需求
+    pykf: "", //培育看法
+    files: [], //用于浏览
+    FilePaths: [], //上传的文件， 有视频和图像
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  //上传照片附件
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+        var tempFilePathDic = {};
+        tempFilePathDic['path'] = res.tempFilePaths[0];
+        tempFilePathDic['type'] = 'photo',
+
+          console.log(typeof tempFilePathDic);
+        that.setData({
+          FilePaths: that.data.FilePaths.concat(tempFilePathDic)
+        });
+
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //上传视频附件
+  chooseVideo: function (e) {
+    var that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: 'back',
+      success: function (res) {
+        var tempFilePathDic = {};
+        tempFilePathDic['path'] = res.tempFilePath;
+        tempFilePathDic['type'] = 'video',
+
+          console.log(typeof tempFilePathDic);
+        that.setData({
+          FilePaths: that.data.FilePaths.concat(tempFilePathDic)
+        });
+
+      }
+    })
+  },
   
+  validate: function () {
+
+    //验证文字
+    for (var key in this.data) {
+      if (key == "__webviewId__") {
+        continue;
+      }
+      if (this.data[key] == [] || this.data[key] == "") {
+        wx.showToast({
+          title: key + " 还没有填写，请补充完整后再提交"
+        })
+        return false;
+      }
+    }
+
+    //验证图片是否上传
+    for (var pic in this.data.FilePaths) {
+      console.log(pic);
+      if (this.data.FilePaths[pic]['type'] == 'photo') {
+        return true;
+      }
+    }
+    wx.showToast({
+      title: '图片没有上传，请至少选择一张图片之后再上传',
+    })
+    return false;
+
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+
+  submitAll: function () {
+    console.log(this.data);
+    var that = this;
+    if (!this.validate()) {
+      return;
+    }
+    
+    wx.request({
+      url: 'https://45053688.hazelnutsgz.com:444/worker/words',
+      method: "POST",
+      data: {
+        zzmc: that.data.zzmc, //组织名称
+        sssq: that.data.sssq, //所属社区
+        gffd: that.data.gffd, //跟访地点
+        gfsj: that.data.gfsj, //跟访时间
+        gfrxm: that.data.gfrxm, //跟访人姓名
+        gfsc: that.data.gfsc, //跟访时长
+        gfxs: that.data.gfxs, //跟访形式
+        hdmc: that.data.hdmc,  //活动名称
+        xmsjh: that.data.xmsjh, //（是）项目书计划
+        fwdx: that.data.fwdx, //（是）服务对象
+        hdmb: that.data.hdmb, //活动目标
+        hdlc: that.data.hdlc, //活动流程
+        yd: that.data.yd,   //优点
+        qd: that.data.qd, //缺点
+        yhjy: that.data.yhjy, //优化建议
+        grgs: that.data.grgs, //个人感受
+        zzzxq: that.data.zzzxq, //自组织需求
+        pykf: that.data.pykf, //培育看法
+        FilePaths: [], //上传的文件， 有视频和图像
+      },
+      success: function (res) {
+        console.log(res);
+        wx.showToast({
+          title: '正在上传图片和视频，请先不要关闭',
+        })
+        var successUp = 0; //成功个数
+        var failUp = 0; //失败个数
+        var length = that.data.FilePaths.length; //总共个数
+        var i = 0; //第几个
+        that.uploadDIY(that.data.FilePaths, successUp, failUp, i, length);
+      },
+      complete: function (res) {
+        console.log("complete");
+      },
+    }),
+      console.log("KKK");
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+
+  uploadDIY(filePaths, successUp, failUp, i, length) {
+    var that = this;
+    console.log("uploadDIY");
+    wx.uploadFile({
+      url: 'https://45053688.hazelnutsgz.com:444/worker/attach',
+      filePath: filePaths[i].path,
+      name: 'file',
+      formData: {
+        'gfrxm': that.data.gfrxm,
+        'gfsj': that.data.gfsj,
+        "type": filePaths[i]['type']
+      },
+      success: (resp) => {
+        successUp++;
+      },
+      fail: (res) => {
+        failUp++;
+      },
+      complete: () => {
+        i++;
+        if (i == length) {
+          wx.showToast({
+            title: '总共' + successUp + '张上传成功, ' + failUp + '张上传失败！',
+          })
+        }
+        else {  //递归调用uploadDIY函数
+          console.log(i);
+          this.uploadDIY(filePaths, successUp, failUp, i, length);
+        }
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+
+
+  pykfChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      pykf: e.detail.value
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  zzzxqChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      zzzxq: e.detail.value
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  grgsChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      grgs: e.detail.value
+    })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  yhjyChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      yhjy: e.detail.value
+    })
+  },
+
+  qdChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      qd: e.detail.value
+    })
+  },
+
+  ydChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      yd: e.detail.value
+    })
+  },
+
+  hdlcChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      hdlc: e.detail.value
+    })
+  },
+
+  hdmbChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      hdmb: e.detail.value
+    })
+  },
+
+  fwdxChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      fwdx: e.detail.value
+    })
+  },
+
+  xmsjhChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      xmsjh: e.detail.value
+    })
+  },
+
+  hdmcChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      hdmc: e.detail.value
+    })
+  },
+
+  gfxsChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      gfxs: e.detail.value
+    })
+  },
+
+  gfscChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      gfsc: e.detail.value
+    })
+  },
+
+  gfrxmChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      gfrxm: e.detail.value
+    })
+  },
+
+  gfsjChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      gfsj: e.detail.value
+    })
+  },
+
+  gfddChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      gfdd: e.detail.value
+    })
+  },
+
+  sssqChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      sssq: e.detail.value
+    })
+  },
+
+
+  zzmcChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      zzmc: e.detail.value
+    })
+  },
+
+  radioxmsjhChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioxmsjhItems;
+
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+    
+    this.setData({
+      xmsjh: e.detail.value,
+      radioxmsjhItems: radioItems
+    });
+  },
+  radiofwdxChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radiofwdxItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      fwdx: e.detail.value,
+      radiofwdxItems: radioItems
+    });
+  },
+
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
   }
-})
+  
+});
